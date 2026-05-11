45e40f3fb288176dc205a01ae762c2c3618c4fe2
AK <akshadakhambalkar1734@gmail.com>
2026-05-11 08:43:22 +0530

    updated message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write("Hello World from Docker 🚀");
+  res.write("CI/CD Pipeline Working Successfully 🚀");
   res.end();
 });
 
