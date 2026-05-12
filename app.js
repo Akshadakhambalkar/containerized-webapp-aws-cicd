391ecce7415df27a130d46d4ffd2347582b9622d
AK <akshadakhambalkar1734@gmail.com>
2026-05-12 11:49:03 +0530

    fix ECS port issue

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send(`
     <html>
       <head>
         <title>AK DevOps</title>
-        <style>
-          body {
-            text-align: center;
-            font-family: Arial;
-            background: linear-gradient(to right, #141e30, #243b55);
-            color: white;
-            padding-top: 100px;
-          }
-          h1 {
-            font-size: 50px;
-          }
-          h2 {
-            color: #00ffcc;
-          }
-          p {
-            font-size: 18px;
-            opacity: 0.8;
-          }
-        </style>
       </head>
-      <body>
+      <body style="text-align:center; font-family:Arial;">
         <h1>🚀 AK DevOps</h1>
         <h2>CI/CD Pipeline Working Successfully</h2>
         <p>Deployed on AWS ECS + ALB + Route 53 + SSL 🔐</p>
-        <p>Version: Updated UI Build ✔</p>
       </body>
     </html>
   `);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
